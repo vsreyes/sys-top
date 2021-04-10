99c9a1a118b5aecd23173da8257bdc37310e617a
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-10 15:18:45 +0200

    CPU warning setup

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -5,6 +5,7 @@ const mem = osu.mem;
 const os = osu.os;
 
 let cpuOverload = 5;
+let alertFrequency = 1;
 
 // Run every 2 seconds
 setInterval(() => {
@@ -13,6 +14,24 @@ setInterval(() => {
     document.getElementById('cpu-usage').innerText = info + '%';
 
     document.getElementById('cpu-progress').style.width = info + '%';
+
+    // Make progress bar red if overload
+    if (info >= cpuOverload) {
+      document.getElementById('cpu-progress').style.background = 'red';
+    } else {
+      document.getElementById('cpu-progress').style.background = '#30c88b';
+    }
+
+    // Check overload
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: 'CPU Overload',
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, 'img', 'icon.png'),
+      });
+
+      localStorage.setItem('lastNotify', +new Date());
+    }
   });
 
   // CPU Free
@@ -46,4 +65,29 @@ function secondsToDhms(seconds) {
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
   return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
+
+// Send notification
+function notifyUser(options) {
+  new Notification(options.title, options);
+}
+
+// Check how much time has passed since notification
+function runNotify(frequency) {
+  if (localStorage.getItem('lastNotify') === null) {
+    // Store timestamp
+    localStorage.setItem('lastNotify', +new Date());
+    return true;
+  }
+
+  const notifyTime = new Date(parseInt(localStorage.getItem('lastNotify')));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60));
+
+  if (minutesPassed > frequency) {
+    return true;
+  } else {
+    return false;
+  }
 }
